29d605576093992879118b19de34fdad6e5f0a5e
Alex-G-R <101558615+Alex-G-R@users.noreply.github.com>
2023-05-19 21:20:54 +0200

    Add files via upload

---
+++ Gra/script.js
@@ -0,0 +1,538 @@
+let input1 = document.getElementById('input1');
+let input01 = document.getElementById('input01');
+
+let input2 = document.getElementById('input2');
+let input02 = document.getElementById('input02');
+
+let input3 = document.getElementById('input3');
+let input03 = document.getElementById('input03');
+
+let input4 = document.getElementById('input4');
+let input04 = document.getElementById('input04');
+
+let input5 = document.getElementById('input5');
+let input05 = document.getElementById('input05');
+
+let input6 = document.getElementById('input6');
+let input06 = document.getElementById('input06');
+
+let input7 = document.getElementById('input7');
+let input07 = document.getElementById('input07');
+
+let input8 = document.getElementById('input8');
+let input08 = document.getElementById('input08');
+
+
+let input11 = document.getElementById('input11');
+let input011 = document.getElementById('input011');
+
+let input12 = document.getElementById('input12');
+let input012 = document.getElementById('input012');
+
+let input13 = document.getElementById('input13');
+let input013 = document.getElementById('input013');
+
+let input14 = document.getElementById('input14');
+let input014 = document.getElementById('input014');
+
+let input15 = document.getElementById('input15');
+let input015 = document.getElementById('input015');
+
+let input16 = document.getElementById('input16');
+let input016 = document.getElementById('input016');
+
+let input17 = document.getElementById('input17');
+let input017 = document.getElementById('input017');
+
+let input18 = document.getElementById('input18');
+let input018 = document.getElementById('input018');
+
+
+input1.addEventListener('change', e => {
+  if (input1.value === 'K Krzyż') {
+    input01.value = '16';
+  }
+  else if (input1.value === 'K Pik') {
+    input01.value = '16';
+  }
+
+  else if (input1.value === 'Q Pik') {
+    input01.value = '12';
+  }
+  else if (input1.value === 'Q Krzyż') {
+    input01.value = '12';
+  }
+
+  else if (input1.value === 'A Pik') {
+    input01.value = '13';
+  }
+  else if (input1.value === 'A Krzyż') {
+    input01.value = '13';
+  }
+
+  else if (input1.value === 'J Pik') {
+    input01.value = '14';
+  }
+  else if (input1.value === 'J Krzyż') {
+    input01.value = '14';
+  }
+});
+
+
+input2.addEventListener('change', e => {
+    if (input2.value === 'K Krzyż') {
+      input02.value = '16';
+    }
+    else if (input2.value === 'K Pik') {
+      input02.value = '16';
+    }
+  
+    else if (input2.value === 'Q Pik') {
+      input02.value = '12';
+    }
+    else if (input2.value === 'Q Krzyż') {
+      input02.value = '12';
+    }
+  
+    else if (input2.value === 'A Pik') {
+      input02.value = '13';
+    }
+    else if (input2.value === 'A Krzyż') {
+      input02.value = '13';
+    }
+  
+    else if (input2.value === 'J Pik') {
+      input02.value = '14';
+    }
+    else if (input2.value === 'J Krzyż') {
+      input02.value = '14';
+    }
+  });
+
+  input3.addEventListener('change', e => {
+    if (input3.value === 'K Krzyż') {
+      input03.value = '16';
+    }
+    else if (input3.value === 'K Pik') {
+      input03.value = '16';
+    }
+  
+    else if (input3.value === 'Q Pik') {
+      input03.value = '12';
+    }
+    else if (input3.value === 'Q Krzyż') {
+      input03.value = '12';
+    }
+  
+    else if (input3.value === 'A Pik') {
+      input03.value = '13';
+    }
+    else if (input3.value === 'A Krzyż') {
+      input03.value = '13';
+    }
+  
+    else if (input3.value === 'J Pik') {
+      input03.value = '14';
+    }
+    else if (input3.value === 'J Krzyż') {
+      input03.value = '14';
+    }
+  });
+
+  input4.addEventListener('change', e => {
+    if (input4.value === 'K Krzyż') {
+      input04.value = '16';
+    }
+    else if (input4.value === 'K Pik') {
+      input04.value = '16';
+    }
+  
+    else if (input4.value === 'Q Pik') {
+      input04.value = '12';
+    }
+    else if (input4.value === 'Q Krzyż') {
+      input04.value = '12';
+    }
+  
+    else if (input4.value === 'A Pik') {
+      input04.value = '13';
+    }
+    else if (input4.value === 'A Krzyż') {
+      input04.value = '13';
+    }
+  
+    else if (input4.value === 'J Pik') {
+      input04.value = '14';
+    }
+    else if (input4.value === 'J Krzyż') {
+      input04.value = '14';
+    }
+  });
+  
+
+
+
+  input5.addEventListener('change', e => {
+    if (input5.value === 'K Krzyż') {
+      input05.value = '16';
+    }
+    else if (input5.value === 'K Pik') {
+      input05.value = '16';
+    }
+  
+    else if (input5.value === 'Q Pik') {
+      input05.value = '12';
+    }
+    else if (input5.value === 'Q Krzyż') {
+      input05.value = '12';
+    }
+  
+    else if (input5.value === 'A Pik') {
+      input05.value = '13';
+    }
+    else if (input5.value === 'A Krzyż') {
+      input05.value = '13';
+    }
+  
+    else if (input5.value === 'J Pik') {
+      input05.value = '14';
+    }
+    else if (input5.value === 'J Krzyż') {
+      input05.value = '14';
+    }
+  });
+  
+  
+  input6.addEventListener('change', e => {
+      if (input6.value === 'K Krzyż') {
+        input06.value = '16';
+      }
+      else if (input6.value === 'K Pik') {
+        input06.value = '16';
+      }
+    
+      else if (input6.value === 'Q Pik') {
+        input06.value = '12';
+      }
+      else if (input6.value === 'Q Krzyż') {
+        input06.value = '12';
+      }
+    
+      else if (input6.value === 'A Pik') {
+        input06.value = '13';
+      }
+      else if (input6.value === 'A Krzyż') {
+        input06.value = '13';
+      }
+    
+      else if (input6.value === 'J Pik') {
+        input06.value = '14';
+      }
+      else if (input6.value === 'J Krzyż') {
+        input06.value = '14';
+      }
+    });
+  
+    input7.addEventListener('change', e => {
+      if (input7.value === 'K Krzyż') {
+        input07.value = '16';
+      }
+      else if (input7.value === 'K Pik') {
+        input07.value = '16';
+      }
+    
+      else if (input7.value === 'Q Pik') {
+        input07.value = '12';
+      }
+      else if (input7.value === 'Q Krzyż') {
+        input07.value = '12';
+      }
+    
+      else if (input7.value === 'A Pik') {
+        input07.value = '13';
+      }
+      else if (input7.value === 'A Krzyż') {
+        input07.value = '13';
+      }
+    
+      else if (input7.value === 'J Pik') {
+        input07.value = '14';
+      }
+      else if (input7.value === 'J Krzyż') {
+        input07.value = '14';
+      }
+    });
+  
+    input8.addEventListener('change', e => {
+      if (input8.value === 'K Krzyż') {
+        input08.value = '16';
+      }
+      else if (input8.value === 'K Pik') {
+        input08.value = '16';
+      }
+    
+      else if (input8.value === 'Q Pik') {
+        input08.value = '12';
+      }
+      else if (input8.value === 'Q Krzyż') {
+        input08.value = '12';
+      }
+    
+      else if (input8.value === 'A Pik') {
+        input08.value = '13';
+      }
+      else if (input8.value === 'A Krzyż') {
+        input08.value = '13';
+      }
+    
+      else if (input8.value === 'J Pik') {
+        input08.value = '14';
+      }
+      else if (input8.value === 'J Krzyż') {
+        input08.value = '14';
+      }
+    });
+    
+
+
+    input12.addEventListener('change', e => {
+        if (input12.value === 'K Serce') {
+          input012.value = '16';
+        }
+        else if (input12.value === 'K Karo') {
+          input012.value = '16';
+        }
+      
+        else if (input12.value === 'Q Serce') {
+          input012.value = '12';
+        }
+        else if (input12.value === 'Q Karo') {
+          input012.value = '12';
+        }
+      
+        else if (input12.value === 'A Serce') {
+          input012.value = '13';
+        }
+        else if (input12.value === 'A Karo') {
+          input012.value = '13';
+        }
+      
+        else if (input12.value === 'J Serce') {
+          input012.value = '14';
+        }
+        else if (input12.value === 'J Karo') {
+          input012.value = '14';
+        }
+      });
+
+      input13.addEventListener('change', e => {
+        if (input13.value === 'K Serce') {
+          input013.value = '16';
+        }
+        else if (input13.value === 'K Karo') {
+          input013.value = '16';
+        }
+      
+        else if (input13.value === 'Q Serce') {
+          input013.value = '12';
+        }
+        else if (input13.value === 'Q Karo') {
+          input013.value = '12';
+        }
+      
+        else if (input13.value === 'A Serce') {
+          input013.value = '13';
+        }
+        else if (input13.value === 'A Karo') {
+          input013.value = '13';
+        }
+      
+        else if (input13.value === 'J Serce') {
+          input013.value = '14';
+        }
+        else if (input13.value === 'J Karo') {
+          input013.value = '14';
+        }
+      });
+
+      input14.addEventListener('change', e => {
+        if (input14.value === 'K Serce') {
+          input014.value = '16';
+        }
+        else if (input14.value === 'K Karo') {
+          input014.value = '16';
+        }
+      
+        else if (input14.value === 'Q Serce') {
+          input014.value = '12';
+        }
+        else if (input14.value === 'Q Karo') {
+          input014.value = '12';
+        }
+      
+        else if (input14.value === 'A Serce') {
+          input014.value = '13';
+        }
+        else if (input14.value === 'A Karo') {
+          input014.value = '13';
+        }
+      
+        else if (input14.value === 'J Serce') {
+          input014.value = '14';
+        }
+        else if (input14.value === 'J Karo') {
+          input014.value = '14';
+        }
+      });
+
+      input15.addEventListener('change', e => {
+        if (input15.value === 'K Serce') {
+          input015.value = '16';
+        }
+        else if (input15.value === 'K Karo') {
+          input015.value = '16';
+        }
+      
+        else if (input15.value === 'Q Serce') {
+          input015.value = '12';
+        }
+        else if (input15.value === 'Q Karo') {
+          input015.value = '12';
+        }
+      
+        else if (input15.value === 'A Serce') {
+          input015.value = '13';
+        }
+        else if (input15.value === 'A Karo') {
+          input015.value = '13';
+        }
+      
+        else if (input15.value === 'J Serce') {
+          input015.value = '14';
+        }
+        else if (input15.value === 'J Karo') {
+          input015.value = '14';
+        }
+      });
+
+      input16.addEventListener('change', e => {
+        if (input16.value === 'K Serce') {
+          input016.value = '16';
+        }
+        else if (input16.value === 'K Karo') {
+          input016.value = '16';
+        }
+      
+        else if (input16.value === 'Q Serce') {
+          input016.value = '12';
+        }
+        else if (input16.value === 'Q Karo') {
+          input016.value = '12';
+        }
+      
+        else if (input16.value === 'A Serce') {
+          input016.value = '13';
+        }
+        else if (input16.value === 'A Karo') {
+          input016.value = '13';
+        }
+      
+        else if (input16.value === 'J Serce') {
+          input016.value = '14';
+        }
+        else if (input16.value === 'J Karo') {
+          input016.value = '14';
+        }
+      });
+
+      input17.addEventListener('change', e => {
+        if (input17.value === 'K Serce') {
+          input017.value = '16';
+        }
+        else if (input17.value === 'K Karo') {
+          input017.value = '16';
+        }
+      
+        else if (input17.value === 'Q Serce') {
+          input017.value = '12';
+        }
+        else if (input17.value === 'Q Karo') {
+          input017.value = '12';
+        }
+      
+        else if (input17.value === 'A Serce') {
+          input017.value = '13';
+        }
+        else if (input17.value === 'A Karo') {
+          input017.value = '13';
+        }
+      
+        else if (input17.value === 'J Serce') {
+          input017.value = '14';
+        }
+        else if (input17.value === 'J Karo') {
+          input017.value = '14';
+        }
+      });
+
+      input18.addEventListener('change', e => {
+        if (input18.value === 'K Serce') {
+          input018.value = '16';
+        }
+        else if (input18.value === 'K Karo') {
+          input018.value = '16';
+        }
+      
+        else if (input18.value === 'Q Serce') {
+          input018.value = '12';
+        }
+        else if (input18.value === 'Q Karo') {
+          input018.value = '12';
+        }
+      
+        else if (input18.value === 'A Serce') {
+          input018.value = '13';
+        }
+        else if (input18.value === 'A Karo') {
+          input018.value = '13';
+        }
+      
+        else if (input18.value === 'J Serce') {
+          input018.value = '14';
+        }
+        else if (input18.value === 'J Karo') {
+          input018.value = '14';
+        }
+      });
+
+      input11.addEventListener('change', e => {
+        if (input11.value === 'K Serce') {
+          input011.value = '16';
+        }
+        else if (input11.value === 'K Karo') {
+          input011.value = '16';
+        }
+      
+        else if (input11.value === 'Q Serce') {
+          input011.value = '12';
+        }
+        else if (input11.value === 'Q Karo') {
+          input011.value = '12';
+        }
+      
+        else if (input11.value === 'A Serce') {
+          input011.value = '13';
+        }
+        else if (input11.value === 'A Karo') {
+          input011.value = '13';
+        }
+      
+        else if (input11.value === 'J Serce') {
+          input011.value = '14';
+        }
+        else if (input11.value === 'J Karo') {
+          input011.value = '14';
+        }
+      });
+      
+  
